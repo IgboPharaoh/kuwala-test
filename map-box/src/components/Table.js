25637438699c96596b6718f8d8afe6b25312f345
IgboPharaoh <87058633+IgboPharaoh@users.noreply.github.com>
2021-11-18 19:39:54 +0100

    added pagination

--- map-box/src/components/Table.js
+++ map-box/src/components/Table.js
@@ -4,77 +4,73 @@ import Data from "./DataContainer";
 import ReactPaginate from "react-paginate";
 
 const Table = () => {
-  const [dataCount, setDatacount] = useState(0);
-  const [   , setPageNum] = useState(0);
   const [perPage] = useState(10);
-  const [data, setData] = useState([]);
+  const [data] = useState(Data);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
-    getData()
-  }, [dataCount]);
+    const endOffset = offset + perPage;
+    setCurrentItems(data.slice(offset, endOffset));
+    setPageCount(Math.ceil(data.length / perPage));
+  }, [offset, perPage, data]);
 
-  const getData =  () => {
-    const allData =  Data;
-    const slice = allData.slice(dataCount, dataCount + perPage);
-    const showData = slice.map((val, key) => (
-      <tr key={key}>
-        <td className="td-1">{val.h3_index}</td>
-        <td className="td-2">{val.kuwala_import_country}</td>
-        <td className="td-1">{val.total}</td>
-        <td className="td-2">{val.children_under_five}</td>
-        <td className="td-1">{val.elderly_60_plus}</td>
-        <td className="td-2">{val.men}</td>
-        <td className="td-1">{val.women}</td>
-        <td className="td-2">{val.women_of_reproductive_age_15_49}</td>
-        <td className="td-1">{val.youth_15_24}</td>
-      </tr>
-    ));
-    setData(showData);
-    setPageNum(Math.ceil(data.length / perPage));
+  const handlePageClick = (e) => {
+    const newOffset = (e.selected * perPage) % data.length;
+    setOffset(newOffset);
   };
-
-  const handlePageClick = (event) => {
-    const selected = event.target;
-    setDatacount(selected + 1);
-  };
-
 
   return (
     <StyledTable>
       <table>
-        <tr className="table-header">
-          <th className="thead-1">h3 Index</th>
-          <th className="thead-2">Kuwala Import Country</th>
-          <th className="thead-1">Total</th>
-          <th className="thead-2">Children Under 5</th>
-          <th className="thead-1">Elderly (60 plus)</th>
-          <th className="thead-2">Men</th>
-          <th className="thead-1">Women</th>
-          <th className="thead-2">Women of Reproductive Age (15-49)</th>
-          <th className="thead-1">Youth (15-24)</th>
-        </tr>
-
-        {data}
-
-        <ReactPaginate
-          previousLabel={"prev"}
-          nextLabel={"next"}
-          breakLabel={"...."}
-          breakClassName={"break-me"}
-          pageCount={dataCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
-        />
+        <thead>
+          <tr className="table-header">
+            <th className="thead-1">h3 Index</th>
+            <th className="thead-2">Kuwala Import Country</th>
+            <th className="thead-1">Total</th>
+            <th className="thead-2">Children Under 5</th>
+            <th className="thead-1">Elderly (60 plus)</th>
+            <th className="thead-2">Men</th>
+            <th className="thead-1">Women</th>
+            <th className="thead-2">Women of Reproductive Age (15-49)</th>
+            <th className="thead-1">Youth (15-24)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((val) => (
+            <tr key={val.h3_index}>
+              <td className="td-1">{val.h3_index}</td>
+              <td className="td-2">{val.kuwala_import_country}</td>
+              <td className="td-1">{val.total}</td>
+              <td className="td-2">{val.children_under_five}</td>
+              <td className="td-1">{val.elderly_60_plus}</td>
+              <td className="td-2">{val.men}</td>
+              <td className="td-1">{val.women}</td>
+              <td className="td-2">{val.women_of_reproductive_age_15_49}</td>
+              <td className="td-1">{val.youth_15_24}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+      <ReactPaginate
+        previousLabel="Prev"
+        nextLabel="Next"
+        breakLabel="...."
+        breakClassName="break-me"
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
+        subContainerClassName="pages pagination"
+        activeClassName="active"
+      />
     </StyledTable>
   );
 };
 
-const StyledTable = styled.table`
+const StyledTable = styled.div`
   width: 100%;
   margin-top: 3rem;
   table {
@@ -102,35 +98,31 @@ const StyledTable = styled.table`
     padding-bottom: 0.5em;
   }
 
-  /* pagination styling */
   .pagination {
-    margin: 1.2rem auto;
     display: flex;
-    justify-content: space-between;
     list-style: none;
     outline: none;
   }
   .pagination a {
-    border-radius: 0.5em;
+    border: 1px solid white;
     padding: 5px 10px;
     outline: none;
     cursor: pointer;
     font-size: 14px;
     font-weight: 500;
     font-family: inherit;
-    /* display: inline-block; */
   }
-  .pagination  a,
+  .pagination a,
   .pagination span {
     color: white;
     background-color: #324960;
   }
-  .pagination a:hover{
+  .pagination a:hover {
     background-color: #4fc3a1;
     color: white;
-    transition-duration:0.1s;
+    transition-duration: 0.1s;
   }
-  .pagination a:active{
+  .pagination a:active {
     background-color: #324960;
   }
 `;
